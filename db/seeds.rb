76e97aa679b7fc94439d7ee7ae22c2a5bac768bd
Janis Vaneylen <janis.vaneylen@gmail.com>
2020-02-20 11:09:31 +0100

    exercise not solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      'Rudi-Dutschke-Straße 1, 10435 Berlin',
+    phone_number:  '+49 151 091 76 151',
+    category:      "chinese",
+  },
+   {
+    name:         'Ranzeria',
+    address:      'Rudi-Dutschke-Straße 2, 10435 Berlin',
+    phone_number:  '+49 151 092 76 152',
+    category:      "italian",
+  },
+   {
+    name:         'Zum Ellbogen',
+    address:      'Rudi-Dutschke-Straße 3, 10435 Berlin',
+    phone_number:  '+49 151 093 76 153',
+    category:      "french",
+  },
+   {
+    name:         'Selling rice for twice the price',
+    address:      'Rudi-Dutschke-Straße 4, 10435 Berlin',
+    phone_number:  '+49 151 094 76 154',
+    category:      "japanese",
+  },
+  {
+    name:         'Pizza East',
+    address:      'Rudi-Dutschke-Straße 5, 10435 Berlin',
+    phone_number:  '+49 151 095 76 155',
+    category:      "belgian",
+
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
